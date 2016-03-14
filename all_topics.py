e0be1f937d4046a8ef22472f76ae7302bb6aa859
Stephen Harding <harding@spawar.navy.mil>
2016-03-13 19:54:15 -0700

    made all_topics.py example more general

--- all_topics.py
+++ all_topics.py
@@ -4,14 +4,30 @@ import dds
 import time
 import json
 import signal
+import sys
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-def print_data(data):
-    print(data['name'])
-    print(json.dumps(data, indent=4), '\n')
+def main(topic_libs):
+    def print_data(data):
+        print(json.dumps(data, indent=4), '\n')
 
-dds.subscribe_to_all_topics(['UCSTopics_C', 'CASHMITopics_C'], print_data)
+    dds.subscribe_to_all_topics(topic_libs,
+                                print_data,
+                                instance_revoked_cb=lambda x: print(x, 'instance revoked ...'),
+                                liveliness_lost_cb=lambda x: print(x, 'liveliness lost ...'),
+    )
 
-while True:
-    time.sleep(100)
+    while True:
+        time.sleep(100)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(
+            'Usage: ' + sys.argv[0] + ' <topic_libraries>\n'
+            '    where topic_libraries is a space delimeted list of\n'
+            '    libraries which define the topics.'
+        )
+        sys.exit(1)
+    main(sys.argv[1:])
