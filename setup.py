84bce1e74e05b3fcf17f98fea28cffa2d20d86d4
Stephen Harding <harding@spawar.navy.mil>
2016-04-22 12:52:05 -0700

    cleaned up

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 setup(name='dds',
       version='1.0',
+      description='python wrapper for RTI DDS',
+      author='Stephen Harding',
+      author_email='stharding@gmail.com',
+      url='https://github.com/stharding/pyDDS',
       py_modules=['dds'],
 )
